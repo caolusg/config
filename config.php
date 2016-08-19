774f6a08df7accba25d7e98cee775fb56218173b
CAO LU <caolu.sg@gmail.com>
2016-08-19 10:56:24 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'ec2db.c0ktzg3ogf9l.ap-southeast-1.rds.amazonaws.com';
+$CFG->dbname    = 'website';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = '19850807';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://54.254.216.147/moodle';
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
